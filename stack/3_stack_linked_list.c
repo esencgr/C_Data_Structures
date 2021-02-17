a66d32766d1b1ab6f16beeeacdc82d02f92de77c
Cagri Esen <esencgr@gmail.com>
2021-02-17 14:09:42 +0300

    add

--- stack/3_stack_linked_list.c
+++ stack/3_stack_linked_list.c
@@ -12,7 +12,7 @@ node *get_node (){
 }
 
 node *push (node *r, int d ){
-    if (r == NULL){
+    if (r == NULL || r -> data == 0){
         r = get_node (r);
         r -> data = d;
         r -> next = NULL;
@@ -28,13 +28,12 @@ node *push (node *r, int d ){
         iter -> next = temp;
         temp -> data = d;
         temp -> next = NULL;
-
         return r;
     }
 }
 
 int pop (node *r){
-    if (r == NULL || r -> data == 0){
+    if (r == NULL || r -> data == 0 ){
         printf ("\nstack empty.\n");
         return 0;          
     }
@@ -56,51 +55,72 @@ int pop (node *r){
     int rval = temp -> data ;
     iter -> next = NULL;
     free (temp);
-
     return rval;
 }
 
 void show (node * r){
     printf ("\n");
-    
     printf ("stack -> ");
     while (r != NULL && r -> data != 0){
         printf ("%d ", r -> data);
         r = r -> next;
     }
-
     printf ("\n");
+}
+
+void show_pop (int val){
+    if (val == 0){
+        printf ("not popped\n");
+    }
+    else{
+        printf ("%d popped\n", val);
+    }
 }
 
 int main (){
     node * stack = NULL;
+    int pop_val;
 
-    printf ("%d popped\n", pop(stack));
+    stack = push (stack, 10);
+    stack = push (stack, 20);
+    stack = push (stack, 30);
+    show (stack);
+    
+    pop_val = pop(stack);
+    show_pop (pop_val);
+    show (stack);
+
+    stack = push (stack, 40);
+    show (stack);
+
+    pop_val = pop(stack);
+    show_pop (pop_val);
+    show (stack);    
+    
+    pop_val = pop(stack);
+    show_pop (pop_val);
+    show (stack);
+    
+    pop_val = pop(stack);
+    show_pop (pop_val);
     show (stack);
 
     stack = push (stack, 10);
     stack = push (stack, 20);
     show (stack);
-    
-    printf ("%d popped\n", pop(stack));
+
+    pop_val = pop(stack);
+    show_pop (pop_val);
     show (stack);
 
-    stack = push (stack, 30);
-    show (stack);
-    
-    printf ("%d popped\n", pop(stack));
+    pop_val = pop(stack);
+    show_pop (pop_val);
     show (stack);
 
-    printf ("%d popped\n", pop(stack));
-    show (stack);
-    printf ("%d popped\n", pop(stack));
-    show (stack);
-
-    printf ("%d popped\n", pop(stack));
+    pop_val = pop(stack);
+    show_pop (pop_val);
     show (stack);
 
-
-    
     return 0;
 }
 
